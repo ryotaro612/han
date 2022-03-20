ee606884ee1f6d53ad4ba89b4f60e31fc3b56736
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-03-20 12:25:08 +0900

    fix test failure.

--- han/vocabulary.py
+++ han/vocabulary.py
@@ -11,6 +11,12 @@ def build_vocabulary(
 
     Each element of `sentences` is a list of words.
 
+    Assign `unknown_index` to a word that the retuened vocabulary
+    don't know. The default value is 0, which may be same as an indice
+    for padding. We use 0 because we have to pass an indice for
+    unknown words to an embedding layer when we train a model while we
+    can feed the words in training dataset to a `Vocabulary`.
+
     """
     vocab: v.Vocab = v.build_vocab_from_iterator(
         (sentence for sentence in sentences)
--- tests/test_vocabulary.py
+++ tests/test_vocabulary.py
@@ -4,6 +4,8 @@ import han.vocabulary as v
 
 class VocabularyTestCase(unittest.TestCase):
     def test(self):
-        sut = v.build_vocabulary([["blue"], ["blue", "glass"]])
+        sut = v.build_vocabulary(
+            [["blue"], ["blue", "glass"]], unknown_index=-1
+        )
         self.assertEqual(set(sut.get_stoi().keys()), set(["blue", "glass"]))
         self.assertEqual(sut["a"], -1)
